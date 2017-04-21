7807e6f1568e995d4ad91be182aec0fea6a88310
Brock Sauvage <bsauvage14@gmail.com>
2017-04-21 14:08:23 -0500

    Done with initialization function

--- buddy.c
+++ buddy.c
@@ -51,6 +51,10 @@
 typedef struct {
 	struct list_head list;
 	/* TODO: DECLARE NECESSARY MEMBER VARIABLES */
+    
+    int block_size;
+    int page_index;
+    char *block_address;
 } page_t;
 
 /**************************************************************************
@@ -82,6 +86,11 @@ void buddy_init()
 	int n_pages = (1<<MAX_ORDER) / PAGE_SIZE;
 	for (i = 0; i < n_pages; i++) {
 		/* TODO: INITIALIZE PAGE STRUCTURES */
+        INIT_LIST_HEAD(&g_pages[i].list);
+        
+        g_pages[i].block_size = -1;
+        g_pages[i].page_index = i;
+        g_pages[i].block_address = PAGE_TO_ADDR(i);
 	}
 
 	/* initialize freelist */
@@ -111,6 +120,11 @@ void *buddy_alloc(int size)
 {
     printStats();
 	/* TODO: IMPLEMENT THIS FUNCTION */
+    if(size <= 0 || size > (1 << MAX_ORDER))
+    {
+        return NULL;
+    }
+    
 	return NULL;
 }
 
